Slicer/libffi

commit 9cc29d04f797000a802b78391e9a36248d7c871d

cmake: Simplify HAVE_AS_X86_PCREL and HAVE_HIDDEN_VISIBILITY_ATTRIBUTE
This commit backports tests from "cmake/ConfigureChecks.cmake"
associated with https://github.com/python-cmake-buildsystem/python-cmake-buildsystem

References:
* https://github.com/python-cmake-buildsystem/python-cmake-buildsystem/issues/135
* https://github.com/python-cmake-buildsystem/python-cmake-buildsystem/commit/c4a7a2ad0823ea49eaf06c6c4e74078193f640c0
  Configure fficonfig.h using system introspection.

configure_options.cmake
@@ -215,21 +215,13 @@ if (NOT MSVC)
         message(STATUS "Checking if .eh_frame section is read-only - no")
     endif()
 
-    execute_process(
-        COMMAND sh -c "echo '.text; foo: nop; .data; .long foo-.; .text' | ${CMAKE_C_COMPILER} ${CMAKE_C_FLAGS} -xassembler -c -o conftest.o - 2>&1"
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        OUTPUT_VARIABLE IGNORE
-        ERROR_VARIABLE IGNORE
-        RESULT_VARIABLE HAVE_AS_X86_PCREL_EXITCODE)
-
-    file(REMOVE ${CMAKE_BINARY_DIR}/conftest.*)
-
-    if(HAVE_AS_X86_PCREL_EXITCODE EQUAL "0")
-        set(HAVE_AS_X86_PCREL 1)
-        message(STATUS "Checking HAVE_AS_X86_PCREL - yes")
-    else()
-        message(STATUS "Checking HAVE_AS_X86_PCREL - no")
-    endif()
+    # Check whether assembler supports PC relative relocs
+    set(check_src [==[
+    int main() {
+        __asm__ __volatile__ (".text; ha: nop; .data; .long ha-.; .text");
+    }
+    ]==])
+    check_c_source_runs("${check_src}" HAVE_AS_X86_PCREL)
 
     execute_process(
         COMMAND
@@ -249,20 +241,15 @@ if (NOT MSVC)
         message(STATUS "Checking HAVE_AS_X86_64_UNWIND_SECTION_TYPE - no")
     endif()
 
-    execute_process(
-        COMMAND sh -c "echo 'int __attribute__ ((visibility (\"hidden\"))) foo(void){return 1;}' | ${CMAKE_C_COMPILER} ${CMAKE_C_FLAGS} -xc -Werror -S -o- - 2>&1 |
-                       grep -q '\\.hidden.*foo'"
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        OUTPUT_VARIABLE IGNORE
-        ERROR_VARIABLE IGNORE
-        RESULT_VARIABLE HAVE_HIDDEN_VISIBILITY_ATTRIBUTE_EXITCODE)
-
-    if(HAVE_HIDDEN_VISIBILITY_ATTRIBUTE_EXITCODE EQUAL "0")
-        set(HAVE_HIDDEN_VISIBILITY_ATTRIBUTE 1)
-        message(STATUS "Checking HAVE_HIDDEN_VISIBILITY_ATTRIBUTE - yes")
-    else()
-        message(STATUS "Checking HAVE_HIDDEN_VISIBILITY_ATTRIBUTE - no")
-    endif()
+    # Check compiler for symbol visibility support
+    set(check_src [==[
+    __attribute__((visibility("hidden"))) int bar(void) {};
+    int main() {bar();}
+    ]==])
+    cmake_push_check_state()
+    set(CMAKE_REQUIRED_FLAGS "-Werror")
+    check_c_source_compiles("${check_src}" HAVE_HIDDEN_VISIBILITY_ATTRIBUTE)
+    cmake_pop_check_state()
 endif()
 
 file(WRITE ${CMAKE_BINARY_DIR}/conftest.c "void nm_test_func(){} int main(){nm_test_func();return 0;}")

commit e8599d9d5a50841c11a5ff8e3438dd12f080b096

cmake: Fix HAVE_AS_X86_PCREL test on macOS ensuring linking is disabled
This fixes the error reported below corresponding to a regression
introduced in 9cc29d04f (cmake: Simplify HAVE_AS_X86_PCREL and
HAVE_HIDDEN_VISIBILITY_ATTRIBUTE)

It also switch from "check_c_source_runs" to "check_c_source_compiles" as
running the executable is not required.

Error associated with the compilation of the check:

  [100%] Linking C executable cmTC_303bd
  [...]
  /Applications/Xcode_11.7.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc  \
    -isysroot /Applications/Xcode_11.7.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk \
    -mmacosx-version-min=10.13 \
    -Wl,-search_paths_first \
    -Wl,-headerpad_max_install_names CMakeFiles/cmTC_303bd.dir/src.c.o -o cmTC_303bd
  ld: in section __DATA,__data reloc 0: X86_64_RELOC_SUBTRACTOR must have r_extern=1 file 'CMakeFiles/cmTC_303bd.dir/src.c.o' for architecture x86_64
  clang: error: linker command failed with exit code 1 (use -v to see invocation)

configure_options.cmake
@@ -227,7 +227,10 @@ if (NOT MSVC)
         __asm__ __volatile__ (".text; ha: nop; .data; .long ha-.; .text");
     }
     ]==])
-    check_c_source_runs("${check_src}" HAVE_AS_X86_PCREL)
+    set(CMAKE_TRY_COMPILE_TARGET_TYPE_SAVED ${CMAKE_TRY_COMPILE_TARGET_TYPE})
+    set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+    check_c_source_compiles("${check_src}" HAVE_AS_X86_PCREL)
+    set(CMAKE_TRY_COMPILE_TARGET_TYPE ${CMAKE_TRY_COMPILE_TARGET_TYPE_SAVED})
 
     # Check whether assembler supports unwind section type
     execute_process(